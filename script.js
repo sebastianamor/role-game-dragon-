52dd1d4b12dbcd53cbe0b3c9c3637215da588a84
¨Jane <smoreira11@hotmail.com>
2024-02-01 22:24:20 +0900

    in java.. not translate

--- script.js
+++ script.js
@@ -40,3 +40,65 @@ const weapons = [
       health: 300
     }
   ]
+
+  const locations = [
+    {
+      name: "town square",
+      "button text": ["Go to store", "Go to cave", "Fight dragon"],
+      "button functions": [goStore, goCave, fightDragon],
+      text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+      name: "store",
+      "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+      "button functions": [buyHealth, buyWeapon, goTown],
+      text: "You enter the store."
+    },
+    {
+      name: "cave",
+      "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+      "button functions": [fightSlime, fightBeast, goTown],
+      text: "You enter the cave. You see some monsters."
+    },
+    {
+      name: "fight",
+      "button text": ["Attack", "Dodge", "Run"],
+      "button functions": [attack, dodge, goTown],
+      text: "You are fighting a monster."
+    },
+    {
+      name: "kill monster",
+      "button text": ["Go to town square", "Go to town square", "Go to town square"],
+      "button functions": [goTown, goTown, goTown],
+      text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+      name: "lose",
+      "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+      "button functions": [restart, restart, restart],
+      text: "You die. ☠️"
+    },
+    { 
+      name: "win", 
+      "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
+      "button functions": [restart, restart, restart], 
+      text: "You defeat the dragon! YOU WIN THE GAME! 🎉" 
+    }
+  ];
+
+  
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
+}
